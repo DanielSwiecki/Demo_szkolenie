fda77ebe964bb592cfc7a82cb2d7dba5289e9729
Daniel Święcki <69246123+DanielSwiecki@users.noreply.github.com>
2025-08-22 00:16:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, Response
-
 app = Flask(__name__)
 
 @app.get("/health")
@@ -11,5 +10,4 @@ def root():
     return Response("Hello Green DevOps (Python)", status=200, mimetype="text/plain")
 
 if __name__ == "__main__":
-    # Flask domyślnie na 5000
     app.run(host="0.0.0.0", port=5000)
