54d77e6ab2c884773f20476ab6e1b399819b4aa5
Daniel Święcki <69246123+DanielSwiecki@users.noreply.github.com>
2025-08-22 11:34:28 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -7,17 +7,22 @@ terraform {
   }
 }
 
-provider "docker" {}          
+provider "docker" {}
 
+# sieć dla środowiska staging, bo Jenkins testuje po nazwie hosta w tej samej sieci
 resource "docker_network" "green_net" {
   name = "green_net"
 }
 
+# kontener staging – używa obrazu zbudowanego wcześniej przez pipeline
 resource "docker_container" "app" {
   name  = "green-app-staging"
   image = "${var.image_name}:${var.tag}"
 
-  ports { internal = 5000, external = 3001 }
+  ports {
+    internal = 5000
+    external = 3001
+  }
 
   networks_advanced {
     name = docker_network.green_net.name
